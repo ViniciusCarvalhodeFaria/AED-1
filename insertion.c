796f280c77b0117f32ca52ce06159e8a82810d70
ViniciusCarvalhodeFaria <vinicius.faria@unifesp.br>
2024-12-03 21:24:34 -0300

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-int const TAM = 400000; 
+int const TAM = 50000;
 
-void insertion(int n, int vet[]){
-    int i,j,x;
-    for(j=1;j<n;j++){
+void insertion(int n, int vet[]) {
+    int i, j, x;
+    for (j = 1; j < n; j++) {
         x = vet[j];
-        for (i=j-1;i>=0 && vet[i]>x; i--){
-            vet[i+1]= vet[i];
+        for (i = j - 1; i >= 0 && vet[i] > x; i--) {
+            vet[i + 1] = vet[i];
         }
-        vet[i+1] = x;
+        vet[i + 1] = x;
     }
 }
 
-int main(){
+int main() {
     int vet[TAM];
-    int n=TAM;
-    int t = clock();
+    int n = TAM;
+
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < TAM; i++) {
+        vet[i] = rand() % TAM;
+    }
+
+    clock_t t = clock();
     insertion(n, vet);
-    t = clock() - t; 
-    printf("Tempo de execucao: %lf s\n", ((double)t)/((CLOCKS_PER_SEC)));
+    t = clock() - t;
+
+    printf("Tempo de execucao: %lf s\n", ((double)t) / CLOCKS_PER_SEC);
     return 0;
 }
