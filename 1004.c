2e4cd7778924bdeda8340953384a4deba839ccbb
ViniciusCarvalhodeFaria <vinicius.faria@unifesp.br>
2025-01-14 20:36:14 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, PROD;
+
+    // Lê os dois números inteiros
+    scanf("%d %d", &A, &B);
+
+    // Calcula o produto
+    PROD = A * B;
+
+    // Exibe o resultado
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
