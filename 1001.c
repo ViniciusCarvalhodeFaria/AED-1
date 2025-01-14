b75a8feab17aac65de7c4e728e6a0c2a604ab1da
ViniciusCarvalhodeFaria <vinicius.faria@unifesp.br>
2025-01-14 20:34:20 -0300

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, X;
+
+    // Lê os dois números inteiros
+    scanf("%d %d", &A, &B);
+
+    // Realiza a soma
+    X = A + B;
+
+    // Exibe o resultado
+    printf("X = %d\n", X);
+
+    return 0;
+}
